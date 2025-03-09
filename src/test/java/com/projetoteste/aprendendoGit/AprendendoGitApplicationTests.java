392eced44cd87c61f9278bb5270c15b633ae743b
amanda vidigal <amandavir06@gmail.com>
2025-03-09 18:47:15 -0300

    commit adicionando projeto spring

---
+++ src/test/java/com/projetoteste/aprendendoGit/AprendendoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoteste.aprendendoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
